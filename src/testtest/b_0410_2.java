2fb7fc30e6bc019816fffaa338ecc39b2298ab20
ptw <mylove7469@naver.com>
2021-04-10 17:35:15 +0900

    14681

--- src/testtest/b_0410_2.java
+++ src/testtest/b_0410_2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class b_0410_2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int h = sc.nextInt();
+		int h = sc.nextInt(); 
 		int m = sc.nextInt();
 
 		if (m < 45) {

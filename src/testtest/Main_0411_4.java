4e126564305d758dce63c2e7ddb58642f451f351
ptw <mylove7469@naver.com>
2021-04-11 02:12:20 +0900

    2438

--- src/testtest/Main_0411_4.java
+++ src/testtest/Main_0411_4.java
@@ -1,24 +1,19 @@
 package testtest;//11021
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main_0411_4{
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine().trim());
-
-		for(int i=1; i<=n; i++) {
-			String num = br.readLine();
-			String[] num2 = num.split(" ");
-			int a = Integer.parseInt(num2[0]);
-			int b = Integer.parseInt(num2[1]);
-			bw.write("Case #"+i+": "+num2[0]+" + "+num2[1]+ " = "+ (a+b) +"\n");
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for(int i=1; i<=a; i++) {
+			for(int j=0; j<i; j++)
+				System.out.print("*");
+			System.out.println("");
+			}
+		
 		}
-		bw.close();
+	
+		
 	}
-}

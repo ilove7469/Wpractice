80ac99943eaf0c9e0416bb4b657f31cae2a3dd94
ptw <mylove7469@naver.com>
2021-04-11 20:04:12 +0900

    10871

---
+++ src/testtest/Main_0411_7.java
@@ -0,0 +1,24 @@
+package testtest;//10871
+
+import java.util.Scanner;
+
+public class Main_0411_7{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int arrNum = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int[] arr = new int[arrNum];
+		
+		for (int i = 0; i < arrNum; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < arrNum; i++) {
+			if (arr[i] < b) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+
+}

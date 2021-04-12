2c733cb0cee0be647cbb4b39ec10a0a810786f0a
ptw <mylove7469@naver.com>
2021-04-12 21:56:40 +0900

    10951

---
+++ src/testtest/Main_0412_1.java
@@ -0,0 +1,19 @@
+package testtest;//10952
+
+import java.util.Scanner;
+
+public class Main_0412_1{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a + b);
+		}
+		
+		
+	}
+
+}

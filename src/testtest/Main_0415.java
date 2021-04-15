6be859418a3033d50f8c4ce348ef0e372aab0195
ptw <mylove7469@naver.com>
2021-04-15 23:27:30 +0900

    진행중

---
+++ src/testtest/Main_0415.java
@@ -0,0 +1,22 @@
+package testtest;//10818
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_0415{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		int[] arr = new int[9];
+
+		for(int i=0; i<9; i++) {
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr);
+	
+		System.out.println(arr[9-1]);
+
+		
+	}
+}

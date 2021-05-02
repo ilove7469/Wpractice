35346cee9f9ed4fa0ff514266cde24ceefd19713
ptw <mylove7469@naver.com>
2021-05-02 20:26:50 +0900

    210502

--- src/baekjoonTest/Baekjoon05.java
+++ src/baekjoonTest/Baekjoon05.java
@@ -1,10 +1,35 @@
 package baekjoonTest;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Baekjoon05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int len = sc.nextInt();
+		int[] aa=new int[len];
+		
+		for(int i=0; i<len; i++) {
+			aa[i]=sc.nextInt();
+		}
+		
+		Arrays.sort(aa);
+		System.out.println(aa[len-1]);
+		
+		double m= (int)aa[len-1];
+		double sum = 0;
+		double avg = 0;
+		
+		
+		for(int i=0; i<len; i++) {
+			sum += (int)aa[i]/m *100;			
+		}
+		
+		avg = sum/len;
+		System.out.println(avg);
+		
 
 	}
-
 }

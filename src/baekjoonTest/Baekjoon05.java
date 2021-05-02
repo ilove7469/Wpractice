89fcd83c8c6219a484926d6de7a63ca03d3d3de0
ptw <mylove7469@naver.com>
2021-05-02 20:37:40 +0900

    210502

--- src/baekjoonTest/Baekjoon05.java
+++ src/baekjoonTest/Baekjoon05.java
@@ -1,35 +1,31 @@
 package baekjoonTest;
 
-import java.sql.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Baekjoon05 {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int len = sc.nextInt();
-		int[] aa=new int[len];
-		
-		for(int i=0; i<len; i++) {
-			aa[i]=sc.nextInt();
+		int[] aa = new int[len];
+
+		for (int i = 0; i < len; i++) {
+			aa[i] = sc.nextInt();
 		}
-		
+
 		Arrays.sort(aa);
-		System.out.println(aa[len-1]);
-		
-		double m= (int)aa[len-1];
+
+		double m = (int) aa[len - 1];
 		double sum = 0;
 		double avg = 0;
-		
-		
-		for(int i=0; i<len; i++) {
-			sum += (int)aa[i]/m *100;			
+
+		for (int i = 0; i < len; i++) {
+			sum += (int) aa[i] / m * 100;
 		}
-		
-		avg = sum/len;
+
+		avg = sum / len;
 		System.out.println(avg);
-		
 
 	}
 }

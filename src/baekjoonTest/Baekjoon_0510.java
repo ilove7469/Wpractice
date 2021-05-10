a9c817d6f072946a3691db79b4dfdcc70a1b0800
ptw <mylove7469@naver.com>
2021-05-10 23:13:09 +0900

    210510

---
+++ src/baekjoonTest/Baekjoon_0510.java
@@ -0,0 +1,16 @@
+package baekjoonTest;
+
+//4673 ม๘วเม฿
+public class Baekjoon_0510 {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static int calculate(int value) {
+		int result = value;
+		
+		return result;
+	}
+
+}

9bbd07e62dab6292b79133048968c89cb0e37a76
ptw <mylove7469@naver.com>
2021-05-11 21:57:06 +0900

    210511

---
+++ src/baekjoonTest/Baekjoon_0511.java
@@ -0,0 +1,23 @@
+package baekjoonTest;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//275
+public class Baekjoon_0511 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[] aa = new int[num];
+
+		for (int i = 0; i < aa.length; i++) {
+			aa[i] = sc.nextInt();
+		}
+		Arrays.sort(aa);
+		for (int j = 0; j < aa.length; j++) {
+			System.out.println(aa[j]);
+		}
+
+	}
+}

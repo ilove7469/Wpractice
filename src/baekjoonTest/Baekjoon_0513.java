13f0a944d8ee71356b375fcae111db51cf926144
ilove7469 <81854016+ilove7469@users.noreply.github.com>
2021-05-13 19:19:34 +0900

    210513
    210513

---
+++ src/baekjoonTest/Baekjoon_0513.java
@@ -0,0 +1,13 @@
+package baekjoonTest;
+//11654
+import java.util.Scanner;
+
+public class Baekjoon_0513 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String aa = sc.nextLine();
+		System.out.println(aa.hashCode());
+	}
+
+}

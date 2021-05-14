9b50385fbd27accc85e2d6b1a9fa42db52bcc676
ptw <mylove7469@naver.com>
2021-05-14 23:47:44 +0900

    210514 진행중

--- src/baekjoonTest/Baekjoon_0514.java
+++ src/baekjoonTest/Baekjoon_0514.java
@@ -26,27 +26,3 @@ public class Baekjoon_0514 {
 
 }
 
-
-/*
-
-import java.util.*;
- 
-public class Main {
-    public static void main(String[] args){
-        
-        Scanner scan = new Scanner(System.in);
-        
-        int num = scan.nextInt();
-        
-        int sum=0;
-        String line = scan.next();
-        
-        for(int i=0; i<num; i++) {            
-            sum += line.charAt(i)-'0';            
-        }
-        
-        System.out.println(sum);
-    }
-}
- 
-  */

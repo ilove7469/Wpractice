b5891db9be792939137c7ed50d0b67b9767397cc
ptw <mylove7469@naver.com>
2021-05-15 15:57:41 +0900

    210515

--- src/baekjoonTest/Baekjoon_0514.java
+++ src/baekjoonTest/Baekjoon_0514.java
@@ -6,47 +6,17 @@ import java.util.Scanner;
 public class Baekjoon_0514 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+  		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		
-		
-		int[] arr= new int[num];
-		String bb = sc.nextLine();
+		String s_arr= sc.next();
 		
 		int result = 0;
-		for(int i=0; i<arr.length; i++) {
-			arr[i]=bb.charAt(i);
-		}
 		
-		for(int j=0; j<arr.length; j++) {
-			result += arr[j];
+		for(int i=0; i<num; i++) {
+			result += s_arr.charAt(i)-'0';
 		}
 		System.out.println(result);
+
 	}
 
 }
-
-
-/*
-
-import java.util.*;
- 
-public class Main {
-    public static void main(String[] args){
-        
-        Scanner scan = new Scanner(System.in);
-        
-        int num = scan.nextInt();
-        
-        int sum=0;
-        String line = scan.next();
-        
-        for(int i=0; i<num; i++) {            
-            sum += line.charAt(i)-'0';            
-        }
-        
-        System.out.println(sum);
-    }
-}
- 
-  */

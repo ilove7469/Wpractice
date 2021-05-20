c1e43e1af986ae8ca4fada1f5654dc266fb3059a
ilove7469 <81854016+ilove7469@users.noreply.github.com>
2021-05-20 22:08:40 +0900

    Create Baekjoon_0520_1.java
    210520

---
+++ src/baekjoonTest/Baekjoon_0520_1.java
@@ -0,0 +1,19 @@
+package baekjoonTest;
+
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+//1152
+public class Baekjoon_0520_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String text = sc.nextLine();
+
+		StringTokenizer st = new StringTokenizer(text, " ");
+		System.out.println(st.countTokens());
+		//text.split(" "); 사용할경우 앞뒤 공백을 자르기위해 trim도 추가해야한다. 
+	}
+}
+

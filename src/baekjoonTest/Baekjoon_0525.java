fe7ff5cf6f48dadb8c662c13f6314f3b40bb398c
ilove7469 <81854016+ilove7469@users.noreply.github.com>
2021-05-25 19:55:52 +0900

    210525

---
+++ src/baekjoonTest/Baekjoon_0525.java
@@ -0,0 +1,16 @@
+package baekjoonTest;//10757
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
+public class Baekjoon_0525 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger num1 = new BigInteger(sc.next());
+		BigInteger num2 = new BigInteger(sc.next());
+		
+		System.out.println(num1.add(num2));
+	}
+}

9c32c63dd500ae92c17c9ecfef66408b2974db06
ptw <mylove7469@naver.com>
2021-09-22 01:16:41 +0900

    210922 미니 프로젝트 테스트중

--- src/test00/test0.java
+++ src/test00/test0.java
@@ -1,13 +1,48 @@
 package test00;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test0 {
 
 	public static void main(String[] args) {
 		
-		method1();
+		/* method1(); */
+		/* method2(); */
+		method3();
 
+	}
+
+	private static void method3() {
+		int[] inputa = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+
+
+		for(int i = 0 ; i < inputa.length ; i++){	
+			arr.add(inputa[i]);
+		}
+		arr.add(0,0);
+	    for(int j = 0 ; j < arr.size() ; j++){
+	        System.out.println(arr.get(j));
+	    }
+		
+	}
+
+	private static void method2() {
+		int[] inputt = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int[] arrA = new int[inputt.length + 1];
+
+		for (int i = 0; i < inputt.length; i++) {
+
+			for (int j = 1; j <= inputt.length; j++) {
+				arrA[j] = inputt[i];
+			}
+
+		}
+
+		for (int k = 0; k < inputt.length; k++) {
+			System.out.println(arrA[k]);
+		}
 	}
 
 	private static void method1() {

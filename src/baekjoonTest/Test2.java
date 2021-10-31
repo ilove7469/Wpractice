9606fbe7348d6f7e0f81d67e85d6a1f908fb653e
ptw <mylove7469@naver.com>
2021-10-31 23:04:02 +0900

    211031 테스트

---
+++ src/baekjoonTest/Test2.java
@@ -0,0 +1,23 @@
+package baekjoonTest;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int a= sc.nextInt();
+			int b = sc.nextInt();
+			if(a==0 && b==0) {
+				break;
+			}
+			System.out.println(a+b);
+		}
+		
+	}
+
+
+
+}

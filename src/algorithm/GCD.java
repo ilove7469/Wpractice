6c082db269225239189f62d87fa43e71a3549961
ptw <mylove7469@naver.com>
2021-12-06 23:00:26 +0900

    공부

--- src/algorithm/GCD.java
+++ src/algorithm/GCD.java
@@ -3,6 +3,9 @@ package algorithm;
 import java.util.Scanner;
 
 //최대공배수 유클리드 호제법 사용
+//호제법이란 말은 두 수가 서로 상대방 수를 나누어서 결국 원하는 수를 얻는 알고리즘
+//https://ko.wikipedia.org/wiki/%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C_%ED%98%B8%EC%A0%9C%EB%B2%95
+
 public class GCD {
 
 	public static void main(String[] args) {
